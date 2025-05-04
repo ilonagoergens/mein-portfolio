2d627a683e60a2c84761d3ec42b2c96f3676368b
ilonagoergens <ilona.goergens@edu.techstarter.de>
2025-05-04 12:01:25 +0200

    Update vite config to copy entire assets folder

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,17 @@ import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-  react(),
-  viteStaticCopy({
-    targets: [
-      {
-        src: path.resolve(__dirname, 'src/assets/Kaiwa.mp4'),
-        dest: 'assets',
-      },
-      {
-        src: path.resolve(__dirname, 'src/assets/dreamsync1.mp4'),
-        dest: 'assets',
-      },
-    ],
-  }),
-],
-base: '/mein-portfolio/',
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, 'src', 'assets'), // Kopiere den gesamten Ordner
+          dest: 'assets',
+        },
+      ],
+    }),
+  ],
+  base: '/mein-portfolio/',
 });
